d0b2bfab5a9d4db70cbc8a50378413c8f97da639
omar <omar.adventure22@gmail.com>
2022-12-17 18:28:11 +0200

    initial trip plan design

--- app/src/main/java/com/example/subway/HomeFragment.java
+++ app/src/main/java/com/example/subway/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.example.subway;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,9 +10,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -21,7 +27,9 @@ public class HomeFragment extends Fragment {
     String[] stations = {"Helwan","Ain-helwan","Hadyek-helwan"};
     AutoCompleteTextView stationsAutoCompleteTxt;
     ArrayAdapter<String> adapterStations;
-
+    private TextView planView;
+    private boolean opened;
+    Button planButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,6 +54,7 @@ public class HomeFragment extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
+
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -69,6 +78,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        //getting start and end station
         stationsAutoCompleteTxt = view.findViewById(R.id.fromStationAutoComplete);
         adapterStations = new ArrayAdapter<String>(getActivity(), R.layout.dropdown_stations,stations);
         stationsAutoCompleteTxt.setAdapter(adapterStations);
@@ -92,6 +102,39 @@ public class HomeFragment extends Fragment {
                 String station = parent.getItemAtPosition(position).toString();
                 Toast.makeText(getActivity(),"station: "+ station, Toast.LENGTH_SHORT).show();
             }
+        });
+        //planing trip
+        planButton = view.findViewById(R.id.planButton);
+        planView = view.findViewById(R.id.tripPlan);
+        planView.setVisibility(View.INVISIBLE);
+
+        ActionBar aBar = getActivity().getActionBar();
+        ColorDrawable cd = new ColorDrawable(Color.parseColor("#FF00FF00"));
+        if (aBar != null) {
+            aBar.setBackgroundDrawable(cd);
+        }
+
+        // using setOnclickListener on button to do hide and show
+        planButton.setOnClickListener(v -> {
+            if (!opened) {
+
+                // visibility of view
+                planView.setVisibility(View.VISIBLE);
+                TranslateAnimation animate = new TranslateAnimation(0, 0, view.getHeight(), 0);
+
+                // duration of animation
+                animate.setDuration(500);
+                animate.setFillAfter(true);
+                planView.startAnimation(animate);
+                planButton.setText("done");
+            } else {
+                planView.setVisibility(View.INVISIBLE);
+                TranslateAnimation animate = new TranslateAnimation(0, 0, 0, view.getHeight());
+                animate.setDuration(0);
+                planView.startAnimation(animate);
+                planButton.setText("Plan");
+            }
+            opened = !opened;
         });
         // Inflate the layout for this fragment
         return view;

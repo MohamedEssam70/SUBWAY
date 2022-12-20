da46c910f2ac332af97ce4f4c56414278ed63aaf
omar <omar.adventure22@gmail.com>
2022-12-20 19:55:27 +0200

    update

--- app/src/main/java/com/example/subway/Login.java
+++ app/src/main/java/com/example/subway/Login.java
@@ -1,11 +1,12 @@
-package com.example.subway;
+package com.example.subway.Activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,14 +14,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.subway.Helpers.Authentication;
+import com.example.subway.MainActivity;
+import com.example.subway.R;
+import com.example.subway.Registration;
+import com.example.subway.ResetPasswordRequest;
+import com.example.subway.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
-    private FirebaseAuth auth ;
     private boolean NoShortageData;
+
+    private FirebaseAuth auth ;
+    private DatabaseReference reference;
+
+    private LinearLayout loginProgress;
+
+    private EditText loginEmail;
+    private EditText loginPassword;
+
+    private TextView registerButton;
+    private TextView forgetPasswordButton;
+    private Button loginButton;
+
+    private String loginIdData;
+    private String loginPasswordData;
 
     // to be tested for preventing login from multiple devices
     /*@Override
@@ -29,88 +55,126 @@ public class Login extends AppCompatActivity {
         if(auth.getCurrentUser() != null){
             Toast.makeText(Login.this, "Already logged in !", Toast.LENGTH_SHORT).show();
         }
-
     }*/
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        auth = FirebaseAuth.getInstance();
-
-        if(auth.getCurrentUser() != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
         setContentView(R.layout.login);
+        /**
+         * Select LinerView of ProgressBar
+         * **/
+        loginProgress = (LinearLayout) findViewById(R.id.loginProgress);
 
-        /*
-        * Sub-Navigation Buttons OnClick Actions
-        * */
+        /**
+         * Select Clickable Items
+         * **/
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (TextView) findViewById(R.id.loginRegisterButton);
+        forgetPasswordButton = (TextView) findViewById(R.id.loginForgetPasswordButton);
 
-        final TextView registerButton = (TextView) findViewById(R.id.loginRegisterButton);
-        final TextView forgetPasswordButton = (TextView) findViewById(R.id.loginForgetPasswordButton);
-        final Button loginButton = (Button) findViewById(R.id.loginButton);
-        final EditText loginEmail = (EditText) findViewById(R.id.loginIDField) ;
-        final EditText loginPassword = (EditText) findViewById(R.id.loginPasswordField) ;
+        /**
+         * Select Data Fields
+         * **/
+        loginEmail = (EditText) findViewById(R.id.loginIDField);
+        loginPassword = (EditText) findViewById(R.id.loginPasswordField);
 
+        /**
+         * Firebase Initialize
+         * * **/
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("user");
 
+        /**
+         * Disable Login Background Process Action
+         * **/
+        onCollectData();
 
+        /**
+         * Login Process
+         * **/
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String loginIdData = loginEmail.getText().toString();
-                final String loginPasswordData = loginPassword.getText().toString();
+                loginIdData = loginEmail.getText().toString();
+                loginPasswordData = loginPassword.getText().toString();
 
                 NoShortageData = Authentication.checkRequiredFields(Login.this, loginIdData, loginPasswordData);
                 if (NoShortageData) {
                     if (Authentication.checkNationalId(loginIdData)) {
                         Toast.makeText(Login.this, "Incorrect National ID", Toast.LENGTH_SHORT).show();
                     }
-                     else {
+                    else {
                         loginUser(loginIdData, loginPasswordData);
                     }
-
-
                 }
             }
         });
+
+        /**
+         * Sub-Navigation Buttons OnClick Actions
+         * **/
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, Registration.class));
             }
         });
-        forgetPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Login.this, "testt", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Login.this, ResetPassword.class));
-            }
+        forgetPasswordButton.setOnClickListener(v -> {
+            startActivity(new Intent(Login.this, ResetPasswordRequest.class));
         });
     }
 
     private void loginUser(String loginEmail, String loginPassword) {
+        onProgress();
         String loginIDEmail = loginEmail + "@metro.eg";
         auth.signInWithEmailAndPassword(loginIDEmail , loginPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-                    String userUID = authResult.getUser().getUid().toString();
-                    Toast.makeText(Login.this, "Successful Login", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
 
-            }
-        })
+                        reference.child(auth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                User user = snapshot.getValue(User.class);
+                                if(user != null) {
+                                    SharedPreferences sharedPreferences = getSharedPreferences("configurations", MODE_PRIVATE);
+                                    SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                                    myEdit.putString("user", user.toJson());
+                                    myEdit.apply();
+                                    Toast.makeText(Login.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(Login.this, "User not found", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                //
+                            }
+                        });
+                    }
+                })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Login.this ,"Wrong Id or Password",Toast.LENGTH_SHORT ).show();
+                        Toast.makeText(Login.this ,"Wrong ID or Password",Toast.LENGTH_SHORT ).show();
+                        onCollectData();
                     }
                 });
+    }
 
+    private void onProgress(){
+        loginProgress.setVisibility(View.VISIBLE);
+        loginEmail.setEnabled(false);
+        loginPassword.setEnabled(false);
+    }
+
+    private void onCollectData(){
+        loginProgress.setVisibility(View.GONE);
+        loginEmail.setEnabled(true);
+        loginPassword.setEnabled(true);
     }
 
 }
